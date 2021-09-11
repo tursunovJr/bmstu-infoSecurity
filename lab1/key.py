58874646c5e790c8a0c454f22a365f8314209812
Jasur <mr.jasur09@mail.ru>
2021-09-12 00:59:45 +0300

    lab1 done

--- lab1/key.py
+++ lab1/key.py
@@ -3,17 +3,17 @@ import subprocess
 import hashlib
 import os.path
 
-def get_checksum():
+def gen_license_key():
     result = subprocess.run("cat /var/lib/dbus/machine-id", stdout=subprocess.PIPE, shell=True, check=True)
     machine_id = result.stdout.decode().strip()
     return hashlib.sha512(machine_id.encode('utf-8')).hexdigest()
 
 
-def check_checksum(filename):
+def check_license(filename):
     if not os.path.isfile(filename):
         return False
     with open(filename, "r") as license:
-        if license.readline() == get_checksum():
+        if license.readline() == gen_license_key():
             return True
         else:
             return False
@@ -21,10 +21,8 @@ def check_checksum(filename):
 
 def create_license(filename):
     with open(filename, "w+") as license:
-        license.write(get_checksum())
+        license.write(gen_license_key())
 
 
 if __name__ == "__main__":
     create_license("license.key")
-    '''with open("license.key", "r") as license:
-        print(license.readline())'''
--- lab1/main.py
+++ lab1/main.py
@@ -1,52 +1,110 @@
-from key import check_checksum
+from key import check_license
 import sys
 from tkinter import *
 from tkinter import messagebox as mb
 import pygame
 import random
+from math import pi
 
-if not check_checksum("license.key"):
-    mb.showerror("error", "U don't have a license 😞 ")
+if not check_license("license.key"):
+    mb.showerror("error", "Access denided! U don't have a license 😞")
     sys.exit()
 else:
     pygame.init()
-    
-    BLACK = [0, 0, 0]
-    WHITE = [255, 255, 255]
-    
-    SIZE = [400, 400]
-    
-    screen = pygame.display.set_mode(SIZE)
-    pygame.display.set_caption("Snow Animation")
-    snow_list = []
-    
-    for i in range(200):
-        x = random.randrange(0, 400)
-        y = random.randrange(0, 400)
-        snow_list.append([x, y])
-    
-    clock = pygame.time.Clock()
-    
+
+    black = [0, 0, 0]
+    white = [255, 255, 255]
+    red = [255, 0, 0]
+    green = [0,255,127]
+    blue = [173,216,230]
+    k = -100
+    i = 1
+    size = [1000, 500]
+    screen = pygame.display.set_mode(size)
+
+    # Устанавливаем заголовок окна
+    pygame.display.set_caption("Человечик")
+
+    def line1(k):
+        pygame.draw.rect(screen, blue, [0, 0, 1000, 250])
+        pygame.draw.rect(screen, green, [0, 250, 1000, 100])
+        pygame.draw.rect(screen, green, [0, 450, 1000, 50])
+        pygame.draw.line(screen, black, [100 + k, 200], [100 + k, 300], 5)
+        pygame.draw.line(screen, black, [50 + k, 400], [100 + k, 300], 5)
+        pygame.draw.line(screen, black, [100 + k, 300], [150 + k, 400], 5)
+        pygame.draw.circle(screen, black, [100 + k, 160], 40, 5)
+        pygame.draw.circle(screen, black, [120 + k, 150], 5)
+        pygame.draw.arc(screen,red,[115+k,160,15,30],pi ,3*pi/2,5)
+        pygame.draw.line(screen, black, [100 + k, 210], [130 + k, 250], 5)
+        pygame.draw.line(screen, black, [130 + k, 250], [160 + k, 270], 5)
+        pygame.draw.line(screen, black, [100 + k, 210], [70 + k, 250], 5)
+        pygame.draw.line(screen, black, [70 + k, 250], [55 + k, 280], 5)
+        pygame.draw.line(screen, black, [0 , 350], [1000, 350], 5)  # road
+        pygame.draw.line(screen, black, [0 , 450], [1000 , 450], 5)  # road
+        k += 10
+        return k
+
+    def line2(k):
+        pygame.draw.rect(screen, blue, [0, 0, 1000, 250])
+        pygame.draw.rect(screen, green, [0, 250, 1000, 100])
+        pygame.draw.rect(screen, green, [0, 450, 1000, 50])
+        pygame.draw.line(screen, black, [100 + k, 200], [100 + k, 300], 5)  # body
+        pygame.draw.circle(screen, black, [100 + k, 160], 40, 5)  # head
+        pygame.draw.circle(screen, black, [120 + k, 150], 5)
+        pygame.draw.arc(screen,red,[115+k,160,15,30],pi ,3*pi/2,5)
+        pygame.draw.line(screen, black, [100 + k, 300], [100 + k, 350], 5)  # left leg part 1
+        pygame.draw.line(screen, black, [100 + k, 350], [50 + k, 380], 5)  # left leg part 2
+        pygame.draw.line(screen, black, [100 + k, 300], [125 + k, 400], 5)  # right leg
+        pygame.draw.line(screen, black, [100 + k, 210], [120 + k, 250], 5)  # right  hand 1
+        pygame.draw.line(screen, black, [120 + k, 250], [150 + k, 280], 5)  # right  hand 1
+        pygame.draw.line(screen, black, [100 + k, 210], [85 + k, 250], 5)  # left  hand 1
+        pygame.draw.line(screen, black, [85 + k, 250], [85 + k, 280], 5)  # left  hand 1
+        pygame.draw.line(screen, black, [0, 350], [1000 , 350], 5)  # road
+        pygame.draw.line(screen, black, [0 , 450], [1000, 450], 5)  # road
+        k+=10
+        return k
+
+    def line3(k):
+        pygame.draw.rect(screen, blue, [0, 0, 1000, 250])
+        pygame.draw.rect(screen, green, [0, 250, 1000, 100])
+        pygame.draw.rect(screen, green, [0, 450, 1000, 50])
+        pygame.draw.line(screen, black, [100 + k, 200], [100 + k, 300], 5)  # body
+        pygame.draw.circle(screen, black, [100 + k, 160], 40, 5)  # head
+        pygame.draw.circle(screen, black, [120 + k, 150], 5)
+        pygame.draw.arc(screen,red,[115+k,160,15,30],pi ,3*pi/2,5)
+        pygame.draw.line(screen, black, [100 + k, 300], [100 + k, 400], 5)  # left leg part
+        pygame.draw.line(screen, black, [100 + k, 300], [125 + k, 350], 5)  # right leg part 1
+        pygame.draw.line(screen, black, [125 + k, 350], [110 + k, 380], 5)  # right leg part 2
+        pygame.draw.line(screen, black, [100 + k, 210], [120 + k, 280], 5)  # right  hand 1
+        pygame.draw.line(screen, black, [100 + k, 210], [80 + k, 280], 5)  # left  hand 1
+        pygame.draw.line(screen, black, [0 , 350], [1000 , 350], 5)  # road
+        pygame.draw.line(screen, black, [0 , 450], [1000 , 450], 5)  # road
+
+        k += 10
+        return k
+
     done = False
-    while not done:
-    
-        for event in pygame.event.get():  
-            if event.type == pygame.QUIT: 
-                done = True   
-    
-        screen.fill(BLACK)
-    
-        for i in range(len(snow_list)):
-    
-            pygame.draw.circle(screen, WHITE, snow_list[i], 2)
-    
-            snow_list[i][1] += 1
-    
-            if snow_list[i][1] > 400:
-                y = random.randrange(-50, -10)
-                snow_list[i][1] = y
-                x = random.randrange(0, 400)
-                snow_list[i][0] = x
-    
+    clock = pygame.time.Clock()
+
+    while done == False:
+        clock.tick(7)
+
+        for event in pygame.event.get():  # Проходимся по событиям
+            if event.type == pygame.QUIT:  # Если пользователь закрыл окно
+                done = True  # Сигнализируем что цикл пора завершать
+
+        screen.fill(white)
+
+        if i == 1:
+            k = line1(k)
+        if i == 2:
+            k = line2(k)
+        if i == 3:
+            k = line3(k)
+            i = 0
+        i+=1
+        if k > 950:
+            k = -100
         pygame.display.flip()
-        clock.tick(60)
+
+    pygame.quit()
